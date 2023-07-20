a9a40848c30f2688cdc27b0d643671f7c0a3819b
djedd1ne <mekki.djallaleddine@gmail.com>
2023-07-20 14:20:04 +0200

    Replace explicit initialization with this-> for constructors Use 2 equavalent ways to call a method for a pointer (-> , (*foo). )

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -1,12 +1,15 @@
 #include "Zombie.hpp"
-#include <iostream>
 
-Zombie::Zombie() : name("Zombie") {}
-Zombie::Zombie(std::string name) : name(name) {}
+Zombie::Zombie() {
+    this->name = "Zombie";
+}
+Zombie::Zombie(std::string name) {
+    this->name = name;
+}
 Zombie::~Zombie() {
     std::cout << name << " is destroyed" << std::endl;
 } 
 
-void Zombie::announce(void){
+void Zombie::announce(void) {
     std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
