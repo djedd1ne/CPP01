008eb69e23f048dc57396164382d5e8b8082955a
djedd1ne <mekki.djallaleddine@gmail.com>
2023-07-30 15:34:22 +0200

    ex04 completed

---
+++ ex04/main.cpp
@@ -0,0 +1,44 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+int main(int ac, char **av){
+    std::string argument1(av[1]);
+    std::string s1(av[2]);
+    std::string s2(av[3]);
+    std::ifstream file_in;
+    std::ofstream file_out;
+    std::stringstream buffer;
+    std::string text;
+    size_t offset;
+
+    if (ac != 4)
+    {
+        std::cout << "Usage: ./sed <filename> string1 string2"
+        << std::endl;
+        return(1);
+    }
+    file_in.open(argument1);
+    if (file_in.is_open())
+    {
+        buffer << file_in.rdbuf();
+        file_in.close();
+        text = buffer.str();
+        offset = 0;
+        while ((offset = text.find(s1), offset) != -1 && text != "\0"){
+            text.erase(offset, s1.length());
+            text.insert(offset,s2);
+            offset += s2.length();
+        }
+        file_out.open(argument1 += ".replace");
+        file_out << text;
+        file_out.close();
+        return (0);
+    }
+    else
+    {
+        std::cout << "input file error" << std::endl;
+        return (1);
+    }
+}
